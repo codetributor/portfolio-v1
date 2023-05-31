340f99b08ff916fff281bbafc1e39ecb0715aff9
Clint Oka <clintoka@Clints-MacBook-Air.local>
2023-05-30 22:48:14 -1000

    animation

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion';
+
 export default function Header () {
     return(
         <div
@@ -7,16 +9,25 @@ export default function Header () {
             <div
             className="flex justify-between"
             >
-                <h1
+                <motion.h1
+                initial={{opacity: 0, translateX: -50}}
+                animate={{opacity: 1, translateX: 0}}
+                transition={{duration: 1.5}}
                 className="text-2xl"
-                >Clint Oka</h1>
+                >Clint Oka</motion.h1>
                 <div className="space-x-5">
-                <button
+                <motion.button
+                    initial={{opacity: 0, translateX: 50}}
+                    animate={{opacity: 1, translateX: 0}}
+                    transition={{duration: 1.5}}
                     className="bg-gray-300 text-blue-500 hover:bg-blue-500 hover:text-white px-3 py-1 rounded-md"
-                    >LinkedIn</button>
-                    <button
+                    >LinkedIn</motion.button>
+                    <motion.button
+                    initial={{opacity: 0, translateX: 50}}
+                    animate={{opacity: 1, translateX: 0}}
+                    transition={{duration: 1.5, delay: .25}}
                     className="bg-gray-300 text-blue-500 hover:bg-blue-500 hover:text-white px-3 py-1 rounded-md"
-                    >Git Hub</button>
+                    >Git Hub</motion.button>
                 </div>
             </div>
             </div>
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion';
+
 export default function Hero() {
     return(
         <div>
@@ -6,21 +8,36 @@ export default function Hero() {
             >
                 
                 
-                <div
+                <motion.div
+                initial={{ opacity: 0, translateX: -200}}
+                animate={{ opacity: 1, translateX: 0}}
+                transition={{ duration: 1.5, type: "spring", ease: "easeInOut", delay: 1}}
                 className="bg-yellow-400 hover:scale-105 transition-all shadow-2xl text-white m-1 md:text-3xl rounded-md px-16 md:px-32 h-12 md:h-24 flex items-center"
-                ><p>Tailwindcss</p></div>
-                <div
+                ><p>Tailwindcss</p></motion.div>
+                <motion.div
+                initial={{ opacity: 0, translateX: -200}}
+                animate={{ opacity: 1, translateX: 0}}
+                transition={{ duration: 1.5, type: "spring", delay: .75}}
                 className="bg-green-400 hover:scale-105 transition-all shadow-2xl text-white m-1 md:text-3xl rounded-md px-32 md:px-64 h-12 md:h-28 flex items-center"
-                ><p>Nextjs</p></div>
-                <div
+                ><p>Nextjs</p></motion.div>
+                <motion.div
+                initial={{ opacity: 0, translateX: -200}}
+                animate={{ opacity: 1, translateX: 0}}
+                transition={{ duration: 1.5, type: "spring", delay: .5}}
                 className="bg-teal-400 hover:scale-105 transition-all shadow-2xl text-white m-1 md:text-3xl rounded-md px-28 md:px-48 h-28 md:h-56 flex items-center"
-                ><p>React</p></div>
-                <div
+                ><p>React</p></motion.div>
+                <motion.div
+                initial={{ opacity: 0, translateX: -200}}
+                animate={{ opacity: 1, translateX: 0}}
+                transition={{ duration: 1.5, type: "spring", delay: .25}}
                 className="bg-blue-400 hover:scale-105 transition-all shadow-2xl text-white m-1 md:text-3xl rounded-md px-24 md:px-60 h-16 md:h-36 flex items-center"
-                ><p>Nodejs | Express</p></div>
-                <div
+                ><p>Nodejs | Express</p></motion.div>
+                <motion.div
+                initial={{ opacity: 0, translateX: -200}}
+                animate={{ opacity: 1, translateX: 0}}
+                transition={{ duration: 1.5, type: "spring"}}
                 className="m-1 shadow-2xl hover:scale-105 transition-all bg-purple-400 text-white rounded-md md:text-3xl px-28 md:px-64 h-20 md:h-48 flex items-center"
-                ><p>Firebase | Solidity</p></div>
+                ><p>Firebase | Solidity</p></motion.div>
             </div>
         </div>
     )
